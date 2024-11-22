eff9218368375def5f2719d4855d23e58a828ffd
henry <hacoronel@sudamericano.edu.ec>
2024-11-22 16:02:25 -0500

    trabajo corregido

--- src/main/kotlin/com/example/crmtwo/repository/CustomerActivityViewRepository.kt
+++ src/main/kotlin/com/example/crmtwo/repository/CustomerActivityViewRepository.kt
@@ -1,8 +1,8 @@
 package com.example.crmtwo.repository
 
-import CustomerActivityView
+import ActivityView
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CustomerActivityViewRepository : JpaRepository<CustomerActivityView, Long>
+interface ActivityView : JpaRepository<ActivityView, Long>
